85c1ef26bd863da74726f952a0d024d12c6dbdfb
Ingy dot Net <ingy@ingy.net>
2011-01-23 12:55:57 +1100

    refactor part 1

--- lib/CogWiki.pm
+++ lib/CogWiki.pm
@@ -1,31 +1,33 @@
 package CogWiki;
 use 5.008003;
 use Mouse;
-extends 'Cog::Plugin';
+extends 'Cog::App';
 
 our $VERSION = '0.02';
 
-sub url_map {
-    [
-        '()',
-        ['/' => 'redirect', ('/pages/')],
-        ['/home/?' => 'home_page'],
-        ['/pages/?' => 'page_list'],
-        ['/page/([A-Z0-9]{4})/?' => 'page_display', ('$1')],
-        ['/page/name/([^/]+)/?' => 'page_by_name', ('$1')],
-        ['/tags/' => 'tag_list' ],
-        ['/tag/([^/]+)/?' => 'tag_page_list', ('$1')],
-    ]
-}
+use constant webapp_class => 'CogWiki::WebApp';
 
-sub site_navigation {
-    [
-        '()',
-        ['Home' => '/home/'],
-        ['Recent Changes' => '/pages/'],
-        ['Tags' => '/tags/'],
-    ]
-}
+package CogWiki::WebApp;
+use Mouse;
+extends 'Cog::WebApp';
+
+use constant url_map => [
+    '()',
+    ['/' => 'redirect', ('/pages/')],
+    ['/home/?' => 'home_page'],
+    ['/pages/?' => 'page_list'],
+    ['/page/([A-Z0-9]{4})/?' => 'page_display', ('$1')],
+    ['/page/name/([^/]+)/?' => 'page_by_name', ('$1')],
+    ['/tags/' => 'tag_list' ],
+    ['/tag/([^/]+)/?' => 'tag_page_list', ('$1')],
+];
+
+use constant site_navigation => [
+    '()',
+    ['Home' => '/home/'],
+    ['Recent Changes' => '/pages/'],
+    ['Tags' => '/tags/'],
+];
 
 1;
 
@@ -38,6 +40,7 @@ CogWiki - Cog Based Wiki Framework
 =head1 SYNOPSIS
 
     > cog init CogWiki
+    > cog update
     > cog make
     > cog start
 
@@ -47,7 +50,7 @@ Ingy döt Net <ingy@cpan.org>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2010. Ingy döt Net.
+Copyright (c) 2010, 2011. Ingy döt Net.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
